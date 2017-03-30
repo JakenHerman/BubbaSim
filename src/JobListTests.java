f41a6bbf6e659968bff3e66573c5bb44253217c6
Jaken Herman <jch053@shsu.edu>
2017-03-29 23:20:13 -0500

    JobList class has all public methods tested

--- src/JobListTests.java
+++ src/JobListTests.java
@@ -26,7 +26,13 @@ public class JobListTests {
 	    JobList jlTest = new JobList();
 	    jlTest.setJobCount(5);
 	    assertEquals(5, jlTest.getJobCount());
+	    
+	    jlTest.setJobCount(18);
+	    assertEquals(18, jlTest.getJobCount());
+	    
 	}
+	
+	
 	
 	@Test
 	public void testGetSizeOfJobList()
@@ -35,4 +41,30 @@ public class JobListTests {
 		jlTest.setJobCount(15);
 		assertEquals(15, jlTest.getSizeOfJobList());
 	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testAddJobs() {
+		JobList jlTest = new JobList();
+		//Should throw NullPointerException,
+		//as setJobCount() was never called.
+		jlTest.addJob(new Job());
+	}
+	
+	@Test
+	public void testGetJobAtIndex(){
+		JobList jlTest = new JobList();
+		jlTest.setJobCount(4);
+		Job test1 = new Job();
+		Job test2 = new Job();
+		
+		test1.setName("T1");
+		test2.setName("T2");
+		
+		jlTest.addJob(new Job());
+		jlTest.addJob(new Job());
+		
+		assert("T1".equals(jlTest.getJobAtIndex(1).getName()));
+		assert("T2".equals(jlTest.getJobAtIndex(2).getName()));
+	}
+
 }
